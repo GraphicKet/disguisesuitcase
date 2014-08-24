96bc2023d9e6b590a4e45b9d4cb49a62aab92e68
GraphicKet <ketchupgaming@outlook.com>
2014-08-24 21:15:03 +0200

    Update shared.lua

--- lua/entities/test_suitcase/shared.lua
+++ lua/entities/test_suitcase/shared.lua
@@ -5,9 +5,9 @@ SUITCASE_changetime = 100
 suitcase_models = {
     ------------------------------ Add models  below here-------------------------------------
 	"models/player/alyx.mdl",
-    "models/player/gman_high.mdl",
-    "models/player/odessa.mdl",
-    "models/player/mossman.mdl",                                
+        "models/player/gman_high.mdl",
+        "models/player/odessa.mdl",
+        "models/player/mossman.mdl",                                
 	"models/player/swat.mdl",
 }
 
@@ -25,6 +25,4 @@ ENT.Category    = "Icejjfish shit"
 ENT.Spawnable		= true 
 ENT.AdminSpawnable	= true 
  
-shit_fucked_up = file.Read("fuckingfilelibrary.txt", "DATA")
-shit_i_guess = util.JSONToTable(shit_fucked_up) -- thanks Crazyscouter for teaching my some of the file library and how to use json
  
